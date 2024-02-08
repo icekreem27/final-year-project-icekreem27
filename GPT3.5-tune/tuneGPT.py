ff612159a26a0cff54807d390b5b45d70df63819
karim abbas <sc21aka@leeds.ac.uk>
2024-02-08 13:37:39 +0000

    added GPT tune

---
+++ GPT3.5-tune/tuneGPT.py
@@ -0,0 +1,29 @@
+# api_key = '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'
+from openai import OpenAI
+
+def create_file(file_path):
+    response = client.files.create(
+        file = open(file_path, "rb"),
+        purpose = "fine-tune"
+    )
+    
+    file_id = response.id
+    return file_id
+
+def create_fine_tune(file_name):
+    client.fine_tuning.jobs.create(
+        training_file = file_name, 
+        model = "gpt-3.5-turbo"
+    )
+
+
+# main
+client = OpenAI()
+file_path = "augmented_dataset.jsonl"
+
+# create file on OpenAI
+fileID = create_file(file_path)
+print("File ID:", fileID)
+
+# create tune job
+create_fine_tune(fileID)
